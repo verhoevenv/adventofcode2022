e4227d1aec72346038df58fb253f41d7c516e748
Vincent Verhoeven <verhoevenv@gmail.com>
2022-12-13 15:00:28 +0100

    Day 13 part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 use std::io::Read;
 
@@ -5,10 +6,10 @@ use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::{take_while, tag};
 use nom::combinator::{map, map_res};
-use nom::multi::{many1, separated_list0};
+use nom::multi::separated_list0;
 use nom::sequence::{delimited, pair, terminated};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Packet {
     List(Vec<Packet>),
     Elem(i32)
@@ -34,17 +35,17 @@ fn parse(input: &str) -> Vec<(Packet, Packet)> {
         alt((list, elem))(input)
     }
     fn all(input: &str) -> IResult<&str, Vec<(Packet, Packet)>> {
-        many1(
-            terminated(
-                pair(
-                    terminated(packet, tag("\n")),
-                    terminated(packet, tag("\n")),
-                    ),
-                tag("\n"))
+        separated_list0(
+            tag("\n"),
+            pair(
+                terminated(packet, tag("\n")),
+                terminated(packet, tag("\n")),
+                ),
         )(input)
     }
 
-    let (_rest_, result) = all(input).unwrap();
+    let (rest, result) = all(input).unwrap();
+    assert!(rest.is_empty());
     return result;
 }
 
@@ -63,50 +64,68 @@ impl TFU {
     }
 }
 
-
-pub fn in_order(packets: Vec<(Packet, Packet)>) -> usize {
-    fn pair_in_order(packets: (Packet, Packet)) -> TFU {
-        use TFU::*;
-        match packets {
-            (Packet::Elem(l), Packet::Elem(r)) => {
-                match l.cmp(&r) {
-                    std::cmp::Ordering::Less => return True,
-                    std::cmp::Ordering::Greater => return False,
-                    std::cmp::Ordering::Equal => return Unknown,
+fn pair_in_order(packets: (&Packet, &Packet)) -> TFU {
+    use TFU::*;
+    match packets {
+        (Packet::Elem(l), Packet::Elem(r)) => {
+            match l.cmp(&r) {
+                Ordering::Less => return True,
+                Ordering::Greater => return False,
+                Ordering::Equal => return Unknown,
+            }
+        },
+        (Packet::List(l), Packet::List(r)) => {
+            let mut l = l.into_iter();
+            let mut r = r.into_iter();
+            loop {
+                let el = l.next();
+                let er = r.next();
+                match (el, er) {
+                    (None, None) => return Unknown,
+                    (None, Some(_)) => return True,
+                    (Some(_), None) => return False,
+                    (Some(pl), Some(pr)) => {
+                        let sub_pair = pair_in_order((pl, pr));
+                        if sub_pair != Unknown {
+                            return sub_pair;
+                        }
+                    },
                 }
-            },
-            (Packet::List(l), Packet::List(r)) => {
-                let mut l = l.into_iter();
-                let mut r = r.into_iter();
-                loop {
-                    let el = l.next();
-                    let er = r.next();
-                    match (el, er) {
-                        (None, None) => return Unknown,
-                        (None, Some(_)) => return True,
-                        (Some(_), None) => return False,
-                        (Some(pl), Some(pr)) => {
-                            let sub_pair = pair_in_order((pl, pr));
-                            if sub_pair != Unknown {
-                                return sub_pair;
-                            }
-                        },
-                    }
-                }
-            },
-            (Packet::List(l), Packet::Elem(r)) => {
-                return pair_in_order((Packet::List(l), Packet::List(vec![Packet::Elem(r)])))
-            },
-            (Packet::Elem(l), Packet::List(r)) => {
-                return pair_in_order((Packet::List(vec![Packet::Elem(l)]), Packet::List(r)))
-            },
-        }
+            }
+        },
+        (Packet::List(l), Packet::Elem(r)) => {
+            return pair_in_order((&Packet::List(l.to_owned()), &Packet::List(vec![Packet::Elem(r.to_owned())])))
+        },
+        (Packet::Elem(l), Packet::List(r)) => {
+            return pair_in_order((&Packet::List(vec![Packet::Elem(l.to_owned())]), &Packet::List(r.to_owned())))
+        },
     }
+}
+
+
+pub fn put_in_order(packets: Vec<(Packet, Packet)>) -> usize {
+    let divider1 = Packet::List(vec![Packet::List(vec![Packet::Elem(2)])]);
+    let divider2 = Packet::List(vec![Packet::List(vec![Packet::Elem(6)])]);
+    let mut all_packets: Vec<_> = packets.into_iter()
+        .flat_map(|(a, b)| vec![a, b])
+        .chain(vec![divider1.clone(), divider2.clone()].into_iter())
+        .collect();
+
+    all_packets.sort_by(|p1, p2| {
+        if pair_in_order((p1, p2)).as_bool() {Ordering::Less} else {Ordering::Greater}
+    });
     
+    let pos1 = all_packets.iter().position(|d| d == &divider1).unwrap() + 1;
+    let pos2 = all_packets.iter().position(|d| d == &divider2).unwrap() + 1;
+
+    return pos1 * pos2;
+}
+
+pub fn sum_in_order(packets: Vec<(Packet, Packet)>) -> usize {
     packets.into_iter()
         .enumerate()
-        .filter_map(|(i, p)| {
-            if pair_in_order(p).as_bool() {Some(i + 1)} else {None}
+        .filter_map(|(i, (p1, p2))| {
+            if pair_in_order((&p1, &p2)).as_bool() {Some(i + 1)} else {None}
         })
         .sum()
 }
@@ -118,7 +137,7 @@ fn main() {
         .read_to_string(&mut input)
         .expect("Failed to read input");
 
-    println!("{}", in_order(parse(&input)));
+    println!("{}", put_in_order(parse(&input)));
 }
 
 
@@ -128,8 +147,13 @@ mod tests {
     use indoc::indoc;
 
     #[test]
-    fn test_in_order() {
-        assert_eq!(in_order(parse(INPUT)), 13);
+    fn test_sum_in_order() {
+        assert_eq!(sum_in_order(parse(INPUT)), 13);
+    }   
+
+    #[test]
+    fn test_put_in_order() {
+        assert_eq!(put_in_order(parse(INPUT)), 140);
     }   
 
     const INPUT: &str = indoc! {"
